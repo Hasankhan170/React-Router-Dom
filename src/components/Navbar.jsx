9f5a920380ad3221cd80ac5689d953723b3dc88f
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-06 09:24:00 -0700

    its working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <>
-        <div>
+    <div>
       <Link to= "">Home</Link>
       <Link to= "About">About</Link>
       <Link to= "Contact">Contact</Link>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
@@ -34,6 +34,6 @@ const router = createBrowserRouter([
 ])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-  </StrictMode>,
+  <RouterProvider router={router}>
+  </RouterProvider>
 )
